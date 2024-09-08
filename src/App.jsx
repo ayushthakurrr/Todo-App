db84a8747604ff4c4abe68b8e011bb448976d2c0
Ayush Thakur <ayushthakur731@gmail.com>
2024-09-08 20:34:03 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Todo } from './Todo'
+const App = () => {
+  return (
+    <div className='flex justify-center items-center w-screen h-screen bg-[#FFCDD2]'>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
